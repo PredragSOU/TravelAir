0e2776cfee16ffce8f92bc80dc91f65333b96fbd
Peconije <predraggrigorov@gmail.com>
2023-08-01 18:09:47 +0200

    Add project files.

---
+++ TravelAir/Areas/Identity/Data/TravelAirDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TravelAir.Areas.Identity.Data;
+
+namespace TravelAir.Data;
+
+public class TravelAirDbContext : IdentityDbContext<ApplicationUser>
+{
+    public TravelAirDbContext(DbContextOptions<TravelAirDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ TravelAir/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const body = document.querySelector("body"),
+      sidebar = document.querySelector(".sidebar"),
+      toggle = document.querySelector(".menu-icon");
+
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
